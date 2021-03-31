cd81a0eaaa9d1e7e6dc7e5763f766dd991809a0a
Lee0701 <hgl1002@naver.com>
2021-03-31 09:14:01 +0900

    Add i18n

---
+++ Leaflet.i18n.alias.php
@@ -0,0 +1,17 @@
+<?php
+
+$specialPageAliases = [];
+
+$specialPageAliases['en'] = [
+	'Map' => [ 'Map' ],
+];
+
+$specialPageAliases['ko'] = [
+	'Map' => [ '지도' ],
+];
+
+$specialPageAliases['ko-kore'] = [
+	'Map' => [ '地圖' ],
+];
+
+?>
